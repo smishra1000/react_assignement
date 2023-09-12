3fe0a5cbd167d8f48e48a3efe445b46c3cbb380a
Sameer Mishra <smishra1000@rediffmail.com>
2023-09-12 12:43:32 +0530

    Create UseLayoutEffect.js

---
+++ UseLayoutEffect.js
@@ -0,0 +1,2 @@
+useLayoutEffect is a React hook that allows you to perform side-effects that rely on the DOM after a component has been rendered but before the browser has had a chance to paint the screen. 
+In this tutorial, we’ll go through an example of using useLayoutEffect to create a simple modal component.
