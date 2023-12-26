2d252ba87d162f77cb92cc8705f08b91aac3bd64
Sameer Mishra <smishra1000@rediffmail.com>
2023-12-26 12:26:46 +0530

    Create CountDownDemo.js

---
+++ CountDownDemo.js
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect, useState } from "react";
+
+export default function Countdown() {
+  const [num, setNum] = useState(100);
+  const [pause, setPause] = useState(false);
+  
+  let intervalRef = useRef();
+  
+  const decreaseNum = () => setNum((prev) => prev - 1);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(decreaseNum, 1000);
+
+    return () => clearInterval(intervalRef.current);
+  }, []);
+  
+  const handleClick = () => {
+    if (!pause) {
+      clearInterval(intervalRef.current);
+    } else {
+      intervalRef.current = setInterval(decreaseNum, 1000);
+    }
+    setPause((prev) => !prev);
+  };
+  
+  return (
+    <div>
+      <div>{num}</div>
+      <button onClick={handleClick}>{pause ? "Run" : "Pause"}</button>
+    </div>
+  );
+}
