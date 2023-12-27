a7cd1a50e9b53d5b814249a983a140c71ab205ed
Sameer Mishra <smishra1000@rediffmail.com>
2023-12-27 09:13:17 +0530

    Update UploadLargeFile.js
    upload large file react node express

--- UploadLargeFile.js
+++ UploadLargeFile.js
@@ -77,3 +77,89 @@ const FileUpload = () => {
 };
 
 export default FileUpload;
+
+
+
+
+
+  Backend----node express 
+
+  const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const multer = require("multer");
+const fs = require("fs");
+
+const app = express();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const PORT = 8000;
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/test", (req, res) => {
+  console.log({ req });
+  res.send("Hello world");
+});
+
+const mergeChunks = async (fileName, totalChunks) => {
+  const chunkDir = __dirname + "/chunks";
+  const mergedFilePath = __dirname + "/merged_files";
+
+  if (!fs.existsSync(mergedFilePath)) {
+    fs.mkdirSync(mergedFilePath);
+  }
+
+  const writeStream = fs.createWriteStream(`${mergedFilePath}/${fileName}`);
+  for (let i = 0; i < totalChunks; i++) {
+    const chunkFilePath = `${chunkDir}/${fileName}.part_${i}`;
+    const chunkBuffer = await fs.promises.readFile(chunkFilePath);
+    writeStream.write(chunkBuffer);
+    fs.unlinkSync(chunkFilePath); // Delete the individual chunk file after merging
+  }
+
+  writeStream.end();
+  console.log("Chunks merged successfully");
+};
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  console.log("Hit");
+  const chunk = req.file.buffer;
+  const chunkNumber = Number(req.body.chunkNumber); // Sent from the client
+  const totalChunks = Number(req.body.totalChunks); // Sent from the client
+  const fileName = req.body.originalname;
+
+  const chunkDir = __dirname + "/chunks"; // Directory to save chunks
+
+  if (!fs.existsSync(chunkDir)) {
+    fs.mkdirSync(chunkDir);
+  }
+
+  const chunkFilePath = `${chunkDir}/${fileName}.part_${chunkNumber}`;
+
+  try {
+    await fs.promises.writeFile(chunkFilePath, chunk);
+    console.log(`Chunk ${chunkNumber}/${totalChunks} saved`);
+
+    if (chunkNumber === totalChunks - 1) {
+      // If this is the last chunk, merge all chunks into a single file
+      await mergeChunks(fileName, totalChunks);
+      console.log("File merged successfully");
+    }
+
+    res.status(200).json({ message: "Chunk uploaded successfully" });
+  } catch (error) {
+    console.error("Error saving chunk:", error);
+    res.status(500).json({ error: "Error saving chunk" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Port listening on ${PORT}`);
+});
